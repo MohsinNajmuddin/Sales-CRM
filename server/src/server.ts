def9a9a94bbc905ea92b76693aa5d6df5854779c
MohsinNajmuddin <mohsin.njm@hotmail.com>
2023-04-25 02:38:21 +0500

    removed unused code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,34 +1,4 @@
-require("dotenv").config();
 import http from "http";
-import { Client } from "@notionhq/client";
-
-// This is Typescript  interface for the shape of the object we will
-// create based on our database to send to the React app
-// When the data is queried it will come back in a much more complicated shape, so our goal is to
-// simplify it to make it easy to work with on the front end
-interface SalesCRM {
-  Name: string;
-  Company: string;
-  Status: string;
-  Priority: string;
-  EstimatedValue: string;
-  AccountOwner: string;
-}
-
-// The dotenv library will read from your .env file into these values on `process.env`
-const notionDatabaseId = process.env.NOTION_DATABASE_ID;
-const notionSecret = process.env.NOTION_SECRET;
-
-// Will provide an error to users who forget to create the .env file
-// with their Notion data in it
-if (!notionDatabaseId || !notionSecret) {
-  throw Error("Must define NOTION_SECRET and NOTION_DATABASE_ID in env");
-}
-
-// Initializing the Notion client with your secret
-const notion = new Client({
-  auth: notionSecret,
-});
 
 const host = "localhost";
 const port = 8000;
