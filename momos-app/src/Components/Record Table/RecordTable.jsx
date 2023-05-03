9cd9b7691593876ad0cd1fd801bf116bae259b12
MohsinNajmuddin <mohsin.njm@hotmail.com>
2023-05-03 09:56:41 +0500

    Added sort and filtering logic

--- momos-app/src/Components/Record Table/RecordTable.jsx
+++ momos-app/src/Components/Record Table/RecordTable.jsx
@@ -5,27 +5,61 @@ import './RecordTable.css';
 const RecordTable = () => {
 
     const [list, setList] = useState([]);
+    const [filterBy, setFilterBy] = useState("");
+    const [sortBy, setSortBy] = useState("Name");
+    const [originalList, setOriginalList] = useState([]);
 
     useEffect(() => {
         fetch("http://localhost:8000/")
         .then((response) => response.json())
         .then((payload) => {
             setList(payload);
+            setOriginalList(payload);
         });
     }, []);
+
+    const handleSort = (column) => {
+        if (column === sortBy) {
+          setList([...list.reverse()]);
+        } else {
+          setList([...list.sort((a, b) => a[column].localeCompare(b[column]))]);
+          setSortBy(column);
+        }
+    };
+
+    const handleFilter = (value) => {
+        setFilterBy(value);
+        if (value.length === 0) {
+            setList(originalList);
+        } else {
+            const filteredList = list.filter((record) =>
+              record.Name.toLowerCase().includes(value.toLowerCase())
+            );
+            setList(filteredList);
+        }
+    };
 
     return (
         <>
             <h1>Sales CRM</h1>
+            <div className="filtering">
+                <label htmlFor="filter">Filter by Name:</label>
+                <input
+                type="text"
+                id="filter"
+                value={filterBy}
+                onChange={(e) => handleFilter(e.target.value)}
+                />
+            </div>
             <table className="record-table">
                 <thead>
                     <tr>
-                    <th>Name</th>
-                    <th>Company</th>
-                    <th>Status</th>
-                    <th>Priority</th>
-                    <th>Estimated Value</th>
-                    <th>Account Owner</th>
+                    <th onClick={() => handleSort("Name")}>Name</th>
+                    <th onClick={() => handleSort("Company")}>Company</th>
+                    <th onClick={() => handleSort("Status")}>Status</th>
+                    <th onClick={() => handleSort("Priority")}>Priority</th>
+                    <th onClick={() => handleSort("EstimatedValue")}>Estimated Value</th>
+                    <th onClick={() => handleSort("AccountOwner")}>Account Owner</th>
                     </tr>
                 </thead>
                 <tbody>
